beb4bbc1e145199de620163b8556b97027fca31f
Rishabh Khanna <60536954+krishabh15@users.noreply.github.com>
2026-09-06 12:16:25 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,37 @@
+// class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         // Base Case where there is either only one element or nums is Empty.
+//         if(nums.length < 2) return false;
+        
+//         // Sort the Array and perform XOR of two elements at a time.
+//         Arrays.sort(nums);  // O(nlogn)
+//         int i = 1;
+//         while(i < nums.length){ // O(n)
+//             if((nums[i-1] ^ nums[i]) == 0) return true;
+//             else i++;
+//         }
+
+//         return false;
+//     }
+// }
+
+// HashSet Solution O(n)
+public class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
+
+// HashSet Count using Streams
+// public class Solution {
+//     public boolean hasDuplicate(int[] nums) {
+//         return Arrays.stream(nums).distinct().count() < nums.length;
+//     }
+// }
